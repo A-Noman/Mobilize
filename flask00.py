cd5c4eedd6271922ec1feb96331647c57ff77de2
Asad Noman <anoman@uncc.edu>
2021-11-18 10:32:32 -0500

    added registration functionality

--- flask00.py
+++ flask00.py
@@ -9,12 +9,16 @@ from flask import redirect, url_for
 from database import db
 from models import Post as Post
 from models import User as User
+from forms import RegisterForm
+import bcrypt
+from flask import session
 
 
 app = Flask(__name__)     # create an app
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mobilizePosts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SECRET_KEY'] = 'SE3155'
 #  Bind SQLAlchemy db object to this Flask app
 db.init_app(app)
 # Setup models
@@ -35,18 +39,14 @@ def signIn():
    a_user =  db.session.query(User).filter_by(email='pyadav5@uncc.edu').one()
    return render_template('signIn.html' )
 
-
-@app.route('/register')
-def register():
-    a_user = db.session.query(User).filter_by(email='pyadav5@uncc.edu').one()
-
-    return render_template('register.html' , user = a_user)
 @app.route('/feed')
 def get_posts():
-   a_user =  db.session.query(User).filter_by(email='pyadav5@uncc.edu').one()
-   my_posts = db.session.query(Post).all()
+    if session.get('user'):
+        my_posts = db.session.query(Post).all()
 
-   return render_template('feed.html', posts=my_posts, user=a_user)
+        return render_template('feed.html', posts=my_posts, user=session['user'])
+    else:
+        return redirect(url_for('signIn'))
 
 
 
@@ -112,9 +112,6 @@ def update_post(post_id):
        return render_template('newPost.html', post=my_post, user=a_user)
 
 
-
-
-
 @app.route('/feed/delete/<post_id>', methods=['POST'])
 def delete_post(post_id):
    # retrieve note from database
@@ -123,6 +120,32 @@ def delete_post(post_id):
    db.session.commit()
 
    return redirect(url_for('get_posts'))
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        # salt and hash password
+        h_password = bcrypt.hashpw(
+            request.form['password'].encode('utf-8'), bcrypt.gensalt())
+        # get entered user data
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        # create user model
+        new_user = User(first_name, last_name, request.form['email'], h_password)
+        # add user to database and commit
+        db.session.add(new_user)
+        db.session.commit()
+        # save the user's name to the session
+        session['user'] = first_name
+        session['user_id'] = new_user.id  # access id value from user model of this newly added user
+        # show user dashboard view
+        return redirect(url_for('get_posts'))
+
+    # something went wrong - display register view
+    return render_template('register.html', form=form)
 
 
 
--- models.py
+++ models.py
@@ -1,30 +1,36 @@
 from database import db
+import datetime
 
 class Post (db.Model):
-
     id = db.Column("id", db.Integer, primary_key=True)
     subject = db.Column("subject", db.String(200))
     text = db.Column("text", db.String(100))
     date = db.Column("date", db.String(50))
     rating = db.Column("rating", db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
-    def __init__(self, subject, text, date):
+    def __init__(self, subject, text, date, rating, user_id):
         self.subject = subject
         self.text = text
         self.date = date
+        self.rating = rating
+        self.user_id = user_id
 
-class User (db.Model):
-
+class User(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column("name", db.String(200))
-    email = db.Column("email", db.String(50))
-    password = db.Column("password", db.String(20))
+    first_name = db.Column("first_name", db.String(100))
+    last_name = db.Column("last_name", db.String(100))
+    email = db.Column("email", db.String(100))
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    posts = db.relationship("Post", backref="user", lazy=True)
 
-    def __init__(self, firstName, lastName, email, password):
-        self.firstName = firstName
-        self.lastName = lastName
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
         self.password = password
+        self.registered_on = datetime.date.today()
 
 
 class Comment (db.Model):
